0e524284bf25b71bfcd61cd6216834c9626430ec
Jim McQuillan <jim@jimmcq.com>
2026-03-13 09:55:02 -0700

    Switch to Prisma Accelerate for Vercel connectivity

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL!;
-  const adapter = new PrismaPg({ connectionString });
-  return new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as { prisma: ReturnType<typeof makePrismaClient> };
+
+function makePrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
 }
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+export const prisma = globalForPrisma.prisma ?? makePrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
